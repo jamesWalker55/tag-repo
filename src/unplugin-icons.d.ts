1a2e62584884920d80be0cff4e6af4f86c68965f
James Walker <james.chunho@gmail.com>
2022-11-17 20:49:06 +0000

    Install and setup 'unplugin-icons'

---
+++ src/unplugin-icons.d.ts
@@ -0,0 +1,14 @@
+/// <reference types="unplugin-icons/types/vue" />
+
+// This fixes type detection on CLion
+
+declare module 'virtual:icons/*' {
+  import type { FunctionalComponent, SVGAttributes } from 'vue'
+  const component: FunctionalComponent<SVGAttributes>
+  export default component
+}
+declare module '~icons/*' {
+  import type { FunctionalComponent, SVGAttributes } from 'vue'
+  const component: FunctionalComponent<SVGAttributes>
+  export default component
+}
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,13 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
+import Icons from 'unplugin-icons/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      Icons(),
+  ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
