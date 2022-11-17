7cfcfed2bb9d9406664bf99866e153af4a53a38b
James Walker <james.chunho@gmail.com>
2022-11-17 20:15:32 +0000

    Install 'vue3-click-away' and enable it on all components

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import {createApp} from 'vue';
 import './style.css';
 import App from './App.vue';
+import VueClickAway from "vue3-click-away";
 
 const app = createApp(App);
+app.use(VueClickAway);
 app.mount('#app');
