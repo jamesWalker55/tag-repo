19e4f60e2d9ccb62b6762c353c3230a4cfe4da4f
James Walker <james.chunho@gmail.com>
2022-11-18 02:03:56 +0000

    Formatter

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import {defineConfig} from 'vite';
 import vue from '@vitejs/plugin-vue';
-import Icons from 'unplugin-icons/vite'
+import Icons from 'unplugin-icons/vite';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
-      vue(),
-      Icons(),
+    vue(),
+    Icons(),
   ],
 
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
