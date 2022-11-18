1859e452f8b96245bb39be5ce0bd547d0e719a60
James Walker <james.chunho@gmail.com>
2022-11-18 02:34:59 +0000

    Install 'window-shadows' and enable shadows for windows

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -3,6 +3,9 @@ all(not(debug_assertions), target_os = "windows"),
 windows_subsystem = "windows"
 )]
 
+use tauri::Manager;
+use window_shadows::set_shadow;
+
 // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
 #[tauri::command]
 fn greet(name: &str) -> String {
@@ -11,6 +14,11 @@ fn greet(name: &str) -> String {
 
 fn main() {
   tauri::Builder::default()
+    .setup(|app| {
+      let window = app.get_window("main").unwrap();
+      set_shadow(&window, true).expect("Unsupported platform!");
+      Ok(())
+    })
     .invoke_handler(tauri::generate_handler![greet])
     .plugin(tauri_plugin_window_state::Builder::default().build())
     .run(tauri::generate_context!())
