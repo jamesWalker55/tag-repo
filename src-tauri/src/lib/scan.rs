3b46bf9af3e49d2df64542d3f0a48bfa888ce2c8
James Walker <james.chunho@gmail.com>
2023-02-13 00:44:33 +0000

    Simplify code with clippy

--- src-tauri/src/lib/scan.rs
+++ src-tauri/src/lib/scan.rs
@@ -16,19 +16,19 @@ pub enum ScanError {
 
 pub fn scan_dir(path: PathBuf) -> Result<Vec<PathBuf>, ScanError> {
   // make sure path is a directory
-  let metadata = path.metadata().map_err(|e| ScanError::IOError(e))?;
+  let metadata = path.metadata().map_err(ScanError::IOError)?;
   if !metadata.is_dir() { return Err(ScanError::NotADirectory); }
 
   let mut items = vec![];
   let mut unscanned_dirs = vec![];
 
   // scan the path for initial list of folders
-  let dir_iter = fs::read_dir(&path).map_err(|e| ScanError::IOError(e))?;
+  let dir_iter = fs::read_dir(&path).map_err(ScanError::IOError)?;
   classify_dir_items(dir_iter, &mut items, &mut unscanned_dirs);
 
   // scan remaining folders
-  while unscanned_dirs.len() > 0 {
-    if let Ok(dir_iter) = fs::read_dir(&unscanned_dirs.pop().unwrap()) {
+  while !unscanned_dirs.is_empty() {
+    if let Ok(dir_iter) = fs::read_dir(unscanned_dirs.pop().unwrap()) {
       classify_dir_items(dir_iter, &mut items, &mut unscanned_dirs);
     }
   }
@@ -37,14 +37,12 @@ pub fn scan_dir(path: PathBuf) -> Result<Vec<PathBuf>, ScanError> {
 }
 
 fn classify_dir_items(dir_iter: ReadDir, items: &mut Vec<PathBuf>, unscanned_dirs: &mut Vec<PathBuf>) {
-  for result in dir_iter {
-    if let Ok(entry) = result {
-      if let Ok(metadata) = entry.metadata() {
-        if metadata.is_dir() {
-          unscanned_dirs.push(entry.path());
-        } else {
-          items.push(entry.path());
-        }
+  for entry in dir_iter.flatten() {
+    if let Ok(metadata) = entry.metadata() {
+      if metadata.is_dir() {
+        unscanned_dirs.push(entry.path());
+      } else {
+        items.push(entry.path());
       }
     }
   }
