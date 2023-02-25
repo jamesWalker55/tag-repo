266cfb606302c373fa0734e148a1aeea9d17f6ff
James Walker <james.chunho@gmail.com>
2023-02-25 22:13:59 +0000

    Fix migration again

--- src-tauri/src/migrations/01u_inital.sql
+++ src-tauri/src/migrations/01u_inital.sql
@@ -1,4 +1,5 @@
 CREATE TABLE items (
+  id INTEGER PRIMARY KEY,
   path TEXT UNIQUE NOT NULL,
   tags TEXT NOT NULL
 );
@@ -6,31 +7,31 @@ CREATE TABLE items (
 -- FTS5 Documentation:
 -- https://www.sqlite.org/fts5.html
 CREATE VIRTUAL TABLE tag_query USING fts5 (
-  -- Include columns to be stored on this virtual table
-  -- I'm including the rowid column so I can match it to the tags, but don't index using FTS
-  item_rowid UNINDEXED,
-  -- Include the tags to index them
-  item_tags,
-
-  -- Use the Unicode61 tokenizer
-  -- https://www.sqlite.org/fts5.html#unicode61_tokenizer
-  tokenize="unicode61",
+  -- Include columns to be stored on this virtual table:
+  -- Include the `id` column so I can join it to `items`, but don't index with FTS
+  id UNINDEXED,
+  -- Include the `tags` column to index them
+  tags,
 
   -- Make this an external content table (don't store the data in this table, but reference
   -- the original table)
-  content=items
---   content_rowid=path,
+  content=items,
+  content_rowid=id,
+
+  -- Use the Unicode61 tokenizer
+  -- https://www.sqlite.org/fts5.html#unicode61_tokenizer
+  tokenize="unicode61"
 );
 
 CREATE TRIGGER items_trigger_ai AFTER INSERT ON items BEGIN
-  INSERT INTO tag_query(item_rowid, item_tags) VALUES (NEW.rowid, NEW.tags);
+  INSERT INTO tag_query(id, tags) VALUES (NEW.id, NEW.tags);
 END;
 
 CREATE TRIGGER items_trigger_ad AFTER DELETE ON items BEGIN
-  INSERT INTO tag_query(tag_query, item_rowid, item_tags) VALUES('delete', OLD.rowid, OLD.tags);
+  INSERT INTO tag_query(tag_query, id, tags) VALUES('delete', OLD.id, OLD.tags);
 END;
 
 CREATE TRIGGER items_trigger_au AFTER UPDATE ON items BEGIN
-  INSERT INTO tag_query(tag_query, item_rowid, item_tags) VALUES('delete', OLD.rowid, old.tags);
-  INSERT INTO tag_query(item_rowid, item_tags) VALUES (NEW.rowid, NEW.tags);
+  INSERT INTO tag_query(tag_query, id, tags) VALUES('delete', OLD.id, old.tags);
+  INSERT INTO tag_query(id, tags) VALUES (NEW.id, NEW.tags);
 END;
