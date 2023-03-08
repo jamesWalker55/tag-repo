28772cf4bc8d0ba5d4b9195195d9034ebcb7a3c8
James Walker <james.chunho@gmail.com>
2023-03-08 02:26:35 +0000

    Combine renames on windows

--- src-tauri/src/watch.rs
+++ src-tauri/src/watch.rs
@@ -1,5 +1,8 @@
-use notify::{Config, RecommendedWatcher, RecursiveMode, Watcher};
-use std::path::Path;
+use notify::event::ModifyKind::Name;
+use notify::event::{ModifyKind, RenameMode};
+use notify::EventKind::Modify;
+use notify::{Config, Event, RecommendedWatcher, RecursiveMode, Watcher};
+use std::path::{Path, PathBuf};
 use tokio::runtime::Handle;
 use tokio::sync::mpsc::unbounded_channel;
 
@@ -18,9 +21,45 @@ async fn async_watch(path: impl AsRef<Path>) -> notify::Result<()> {
 
     watcher.watch(path.as_ref(), RecursiveMode::Recursive)?;
 
-    while let Some(res) = rx.recv().await {
-        let res = res.unwrap();
-        println!("{:?}", res);
+    let mut last_rename_from: Option<PathBuf> = None;
+
+    while let Some(evt) = rx.recv().await {
+        let evt = evt.unwrap();
+        if let Event {
+            kind: Modify(Name(RenameMode::From)),
+            mut paths,
+            ..
+        } = evt
+        {
+            let path = paths.pop().unwrap();
+            last_rename_from = Some(path);
+            continue;
+        } else if let Event {
+            kind: Modify(Name(RenameMode::To)),
+            mut paths,
+            ..
+        } = evt
+        {
+            let from_path = last_rename_from
+                .take()
+                .expect("Got 'Rename To' event, but no 'Rename From' event happened before this!");
+            let to_path = paths.pop().unwrap();
+            let evt = Event {
+                kind: Modify(Name(RenameMode::Both)),
+                paths: vec![from_path, to_path],
+                attrs: evt.attrs.clone(),
+            };
+            println!("{:?}", evt);
+        } else if let Event {
+            kind: Modify(ModifyKind::Any),
+            ..
+        } = evt
+        {
+            // ignore
+            // println!("{:?}", evt);
+        } else {
+            println!("{:?}", evt);
+        }
     }
 
     Ok(())
