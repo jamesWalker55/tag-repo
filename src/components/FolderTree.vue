5e7c47998314ce513cb99c569cf72d6a759da2e7
James Walker <james.chunho@gmail.com>
2023-04-14 03:48:43 +0100

    remove alpha text

--- src/components/FolderTree.vue
+++ src/components/FolderTree.vue
@@ -74,7 +74,7 @@ const log = console.log;
     >
       <DirTreeIcon class="h-[20px] w-[20px] flex-none text-neutral-600" />
       <div class="min-w-0 flex-1 truncate whitespace-nowrap">
-        Folders (Alpha)
+        Folders
       </div>
       <VerticalDots
         class="box-content h-16px w-16px flex-none cursor-pointer rounded p-0.5 text-neutral-600 hover:bg-neutral-200 hover:text-neutral-800"
