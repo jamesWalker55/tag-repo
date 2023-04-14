9ff594a2517cabb734ef3fa4e7b20b0fb05e4635
James Walker <james.chunho@gmail.com>
2023-04-14 02:46:45 +0100

    styling for disabled menu items

--- src/components/menu/MenuItem.vue
+++ src/components/menu/MenuItem.vue
@@ -11,21 +11,35 @@ defineProps<Props>();
 </script>
 
 <template>
-  <tr class="group">
-    <td class="pl-3 group-hover:bg-neutral-100">
-      <slot name="icon" v-bind="{ defaultClasses: 'text-neutral-600' }"></slot>
+  <tr class="group" :class="!disabled ? '' : 'text-neutral-400'">
+    <td class="pl-3" :class="!disabled ? 'group-hover:bg-neutral-100' : ''">
+      <slot name="icon" v-bind="{ defaultClasses: !disabled ? 'text-neutral-600' : 'text-neutral-400' }"></slot>
     </td>
     <td
-      class="flex h-8 items-center whitespace-nowrap px-3 group-hover:bg-neutral-100"
+      class="flex h-8 items-center whitespace-nowrap px-3"
+      :class="!disabled ? 'group-hover:bg-neutral-100' : ''"
     >
       {{ text }}
     </td>
-    <td class="pr-3 text-right group-hover:bg-neutral-100">{{ altText }}</td>
-    <td v-if="$slots.default" class="pr-3 group-hover:bg-neutral-100">
+    <td
+      class="pr-3 text-right"
+      :class="!disabled ? 'group-hover:bg-neutral-100' : ''"
+    >
+      {{ altText }}
+    </td>
+    <td
+      v-if="$slots.default"
+      class="pr-3"
+      :class="!disabled ? 'group-hover:bg-neutral-100' : ''"
+    >
       <MenuMore />
       <!-- temporary, implement this when you have time -->
       <slot />
     </td>
-    <td v-else class="pr-3 group-hover:bg-neutral-100"></td>
+    <td
+      v-else
+      class="pr-3"
+      :class="!disabled ? 'group-hover:bg-neutral-100' : ''"
+    ></td>
   </tr>
 </template>
