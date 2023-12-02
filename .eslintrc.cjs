02e992a2f67362f3b235258c84a8891dbcb142ac
James Walker <james.chunho@gmail.com>
2023-12-02 18:56:47 +0800

    Configure linters to allow unused parameters in functions

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -25,5 +25,10 @@ module.exports = {
     sourceType: "module",
   },
   plugins: ["@typescript-eslint", "vue"],
-  rules: {},
+  rules: {
+    // https://typescript-eslint.io/rules/no-unused-vars/
+    // Note: you must disable the base rule as it can report incorrect errors
+    "no-unused-vars": "off",
+    "@typescript-eslint/no-unused-vars": ["error", { args: "none" }],
+  },
 };
